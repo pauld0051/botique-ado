dab0f7e4e18ba86c138ba64cb65809ef2281232e
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-04-02 11:52:14 +0200

    Added country field select option to checkout

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -29,7 +29,6 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
-
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
