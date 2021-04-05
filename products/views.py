d93d624e348d4ab78125e6eddea4b936e74b06ac
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-04-05 11:23:58 +0200

    Add delete functionality to product handling

--- products/views.py
+++ products/views.py
@@ -98,9 +98,9 @@ def edit_product(request, product_id):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully updated product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+           product = form.save()
+           messages.success(request, 'Successfully added product!')
+           return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request, 'Failed to update product. Please ensure the form is valid.')
@@ -115,3 +115,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
